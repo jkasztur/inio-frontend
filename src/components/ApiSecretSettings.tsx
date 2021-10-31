9bbab6e8043b1c62da5905d98bc0fcee011ef736
Jan <jan.kasztura@seznam.cz>
2021-10-31 16:30:26 +0100

    simplify settings

---
+++ src/components/ApiSecretSettings.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import MenuItem from './MenuItem'
+import { useCookies } from 'react-cookie'
+import { useState } from 'react';
+import { getAxios } from '../axios/client';
+import { AxiosError } from 'axios';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { ErrorMessage } from './ErrorMessage';
+
+export default function ApiSecretSettings(props: { path: string, name: string }) {
+	const [apiKey, setApiKey] = useState("");
+	const [secret, setSecret] = useState("");
+	const [isSubmitted, setIsSubmitted] = useState(false);
+	const [error, setError] = useState("");
+	const [cookies] = useCookies(['accessToken', 'accountId'])
+
+	function handleChangeApiKey(e: any) {
+		setIsSubmitted(false);
+		setApiKey(e.target.value);
+	}
+
+	function handleChangeSecret(e: any) {
+		setIsSubmitted(false);
+		setSecret(e.target.value);
+	}
+
+	function handleSubmit(event: any) {
+		event.preventDefault();
+		if (apiKey.length > 0 && secret.length > 0) {
+			const postData = async () => {
+				try {
+					await getAxios().post(props.path, {
+						apiKey: apiKey,
+						secret: secret,
+					}, {
+						headers: {
+							'x-access-token': cookies.accessToken,
+							'x-account-id': cookies.accountId
+						},
+						withCredentials: true
+					});
+					setIsSubmitted(true);
+					setError('')
+				} catch (err) {
+					setError(
+						`message= ${(err as AxiosError).message}, data= ${JSON.stringify(
+							(err as AxiosError).response?.data
+						)}`
+					);
+				}
+			};
+			postData();
+		} else {
+			setError("Some input is empty");
+		}
+	}
+
+	return (
+		<>
+			<h1>{props.name}</h1>
+			<Box
+				component='form'
+				noValidate
+				autoComplete="off"
+				onSubmit={handleSubmit}
+				sx={{
+					'& .MuiTextField-root': { m: 1, width: '25ch' },
+				}}>
+				<TextField
+					required
+					id="outlined-required"
+					label="API Key"
+					onChange={handleChangeApiKey}
+				/>
+				<div />
+				<TextField
+					required
+					id="outlined-required"
+					label="Secret"
+					onChange={handleChangeSecret}
+				/>
+				<div />
+				<Button variant="outlined" type='submit'>Submit</Button>
+			</Box>
+			{isSubmitted && (
+				<>
+					<div />
+					<label style={{ color: 'green' }}>Successfully modified {props.name} settings!</label>
+				</>
+			)}
+			{error.length > 0 && <ErrorMessage message={error} />}
+		</>
+	);
+}
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,124 +1,15 @@
-import { AxiosError } from "axios";
 import React from "react";
-import { useState } from "react";
-import { useCookies } from "react-cookie";
-import { getAxios } from "../axios/client";
-import { ErrorMessage } from "../components/ErrorMessage";
+import ApiSecretSettings from '../components/ApiSecretSettings';
 
 export const Settings: React.FC = () => {
-	const [krakenApiKey, setKrakenApiKey] = useState("");
-	const [krakenSecret, setKrakenSecret] = useState("");
-	const [binanceApiKey, setBinanceApiKey] = useState("");
-	const [binanceSecret, setBinanceSecret] = useState("");
-	const [isSubmittedKraken, setIsSubmittedKraken] = useState(false);
-	const [isSubmittedBinance, setIsSubmittedBinance] = useState(false);
-	const [error, setError] = useState("");
-	const [cookies] = useCookies(['accessToken', 'accountId'])
-
-	function handleChangeKrakenApiKey(e: any) {
-		setIsSubmittedKraken(false);
-		setKrakenApiKey(e.target.value);
-	}
-
-	function handleChangeKrakenSecret(e: any) {
-		setIsSubmittedKraken(false);
-		setKrakenSecret(e.target.value);
-	}
-
-	function handleChangeBinanceApiKey(e: any) {
-		setIsSubmittedBinance(false);
-		setBinanceApiKey(e.target.value);
-	}
-
-	function handleChangeBinanceSecret(e: any) {
-		setIsSubmittedBinance(false);
-		setBinanceSecret(e.target.value);
-	}
-
-	function handleSubmit(event: any, path: string, apiKey: string, secret: string, setIsSubmitedFn: (data: any) => void) {
-		event.preventDefault();
-		if (apiKey.length > 0 && secret.length > 0) {
-			const postData = async () => {
-				try {
-					await getAxios().post(path, {
-						apiKey: apiKey,
-						secret: secret,
-					}, {
-						headers: {
-							'x-access-token': cookies.accessToken,
-							'x-account-id': cookies.accountId
-						},
-						withCredentials: true
-					});
-					setIsSubmitedFn(true);
-					setError('')
-				} catch (err) {
-					setError(
-						`message= ${(err as AxiosError).message}, data= ${JSON.stringify(
-							(err as AxiosError).response?.data
-						)}`
-					);
-				}
-			};
-			postData();
-		} else {
-			setError("Some input is empty");
-		}
-	}
-	function handleSubmitKraken(event: any) {
-		handleSubmit(event, '/kraken/setup', krakenApiKey, krakenSecret, setIsSubmittedKraken)
-	}
-	function handleSubmitBinance(event: any) {
-		handleSubmit(event, '/binance/setup', binanceApiKey, binanceSecret, setIsSubmittedBinance)
-	}
-
 
 	return (
 		<div style={{ flexDirection: 'column' }}>
 			<h2>Settings</h2>
-			<h1>Kraken</h1>
-			<form onSubmit={handleSubmitKraken}>
-				<label>
-					API key:
-					<input type="text" name="apiKey" onChange={handleChangeKrakenApiKey} />
-				</label>
-				<div />
-				<label>
-					Secret:
-					<input type="text" name="secret" onChange={handleChangeKrakenSecret} />
-				</label>
-				<div />
-				<input type="submit" value="Submit" />
-			</form>
-			{isSubmittedKraken && (
-				<>
-					<div />
-					<label style={{ color: 'green' }}>Successfully modified Kraken settings!</label>
-				</>
-			)}
+
+			<ApiSecretSettings path='/kraken/setup' name='Kraken' />
 			<div />
-			<h1>Binance</h1>
-			<form onSubmit={handleSubmitBinance}>
-				<label>
-					API key:
-					<input type="text" name="apiKey" onChange={handleChangeBinanceApiKey} />
-				</label>
-				<div />
-				<label>
-					Secret:
-					<input type="text" name="secret" onChange={handleChangeBinanceSecret} />
-				</label>
-				<div />
-				<input type="submit" value="Submit" />
-			</form>
-			{isSubmittedBinance && (
-				<>
-					<div />
-					<label style={{ color: 'green' }}>Successfully modified Binance settings!</label>
-				</>
-			)}
-			<div />
-			{error.length > 0 && <ErrorMessage message={error} />}
+			<ApiSecretSettings path='/binance/setup' name='Binance' />
 		</div>
 	);
 };
