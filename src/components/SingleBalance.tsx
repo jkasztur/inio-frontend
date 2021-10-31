a6b3f6146ba96af2d4b8f802fe0d353df32d4a2f
Jan <jan.kasztura@seznam.cz>
2021-10-31 17:18:34 +0100

    unify balance component

---
+++ src/components/SingleBalance.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { getAxios } from '../axios/client';
+
+export default function SingleBalance(props: { path: string, name: string, currency: string }) {
+	const [balance, setBalance] = useState('0');
+	const [cookies] = useCookies(['accessToken', 'accountId'])
+
+	async function fetchData() {
+		try {
+			setBalance(`Loading...`);
+			const response = await getAxios().get(props.path, {
+				headers: {
+					'x-access-token': cookies.accessToken,
+					'x-account-id': cookies.accountId
+				},
+				params: {
+					currency: props.currency
+				},
+				withCredentials: true
+			});
+			const { amount, currency } = response.data;
+			setBalance(`${amount} ${currency}`);
+		} catch (err) {
+			setBalance(`0 ${props.currency}`);
+		}
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [props.currency]);
+
+	return (
+		<>
+			<h3>{props.name}</h3>
+			<div />
+			<h3>{balance}</h3>
+		</>
+	)
+}
--- src/pages/Balances.tsx
+++ src/pages/Balances.tsx
@@ -4,36 +4,10 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import { useCookies } from "react-cookie";
 import { getAxios } from "../axios/client";
+import SingleBalance from "../components/SingleBalance";
 
 export const Balances: React.FC = () => {
-	const [krakenBalance, setKrakenBalance] = useState('0');
-	const [binanceBalance, setBinanceBalance] = useState('0');
 	const [mainCurrency, setMainCurrency] = useState('CZK');
-	const [cookies] = useCookies(['accessToken', 'accountId'])
-
-	async function fetchData(path: string, setBalanceFn: (value: any) => void) {
-		try {
-			setBalanceFn(`Loading...`);
-			const response = await getAxios().get(path, {
-				headers: {
-					'x-access-token': cookies.accessToken,
-					'x-account-id': cookies.accountId
-				},
-				params: {
-					currency: mainCurrency
-				},
-				withCredentials: true
-			});
-			const { amount, currency } = response.data;
-			setBalanceFn(`${amount} ${currency}`);
-		} catch (err) {
-			setBalanceFn(`0 ${mainCurrency}`);
-		}
-	}
-	useEffect(() => {
-		fetchData('/kraken/balance', setKrakenBalance);
-		fetchData('/binance/balance', setBinanceBalance);
-	}, [mainCurrency]);
 
 	return (
 		<div style={{ flexDirection: 'column' }}>
@@ -54,13 +28,9 @@ export const Balances: React.FC = () => {
 					<MenuItem value={'EUR'}>EUR</MenuItem>
 				</Select>
 			</FormControl>
-			<h3>Kraken</h3>
-			<div />
-			<h3>{krakenBalance}</h3>
-			<div />
-			<h3>Binance</h3>
-			<div />
-			<h3>{binanceBalance}</h3>
+			<SingleBalance name='Kraken' path='/kraken/balance' currency={mainCurrency} />
+			<SingleBalance name='Binance' path='/binance/balance' currency={mainCurrency} />
+			<SingleBalance name='ETH Chain' path='/eth/balance' currency={mainCurrency} />
 		</div>
 	);
 };
